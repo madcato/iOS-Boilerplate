1a75160276bcf5e623acb845440518951b44e2ad
Daniel Vela Angulo <dvela@atsistemas.com>
2018-11-21 10:34:01 +0100

    Initial Commit

---
+++ iOS-Boilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Boilerplate
+//
+//  Created by Daniel Vela Angulo on 21/11/2018.
+//  Copyright © 2018 veladan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
