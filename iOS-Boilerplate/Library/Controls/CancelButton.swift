68ea79244e3630d00956e53fc97d350c95834a53
Daniel Vela Angulo <dvela@atsistemas.com>
2018-12-20 12:46:34 +0100

    Add Theme classes

---
+++ iOS-Boilerplate/Library/Controls/CancelButton.swift
@@ -0,0 +1,25 @@
+//
+//  NPDCancelButton.swift
+//  iOS-Boilerplate
+//
+//  Created by Daniel Vela on 20/12/18.
+//  Copyright © 2018 veladan. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Use this class to set it in the storyboard, in the Identity Inspector, in Custom class.
+ This way, every cancel button with this class setted has the style defined in the Theme class.
+ */
+class CancelButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- iOS-Boilerplate/Startup/AppDelegate.swift
+++ iOS-Boilerplate/Startup/AppDelegate.swift
@@ -37,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         enableNetworkIndicator()
+        configureAppearance()
         #if DEBUG
         condifgureFLEXLauncher()
         #endif
