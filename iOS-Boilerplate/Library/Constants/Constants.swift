078f046fabf18f85c9eb4678de88b7bd5efd185b
Daniel Vela <veladan@me.com>
2021-06-08 16:54:15 +0200

    add constants

---
+++ iOS-Boilerplate/Library/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  iOS-Boilerplate
+//
+//  Created by Daniel Vela on 8/6/21.
+//  Copyright © 2021 veladan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let numberOfVisibleLines = 6
+    static let fifteenMinutesMillis = 15 * 60 * 1000
+}
